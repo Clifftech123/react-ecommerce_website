f75793d24feb5e1146dc385952be3f9703731769
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-09-21 22:33:58 +0000

    I added Routing futuer to some components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import NavBarComponent from './Components/navbarComponent';
+import Logins from "./Account/Login";
+import Signup from './Account/Singnup';
 
-import './App.css';
-import HomePage from './Components/HomePage';
+
 
 const App =() => {
   return (
 		<>
-	    <HomePage/>
+			<BrowserRouter>
+				<div className=''>
+					<NavBarComponent />
+				</div>
+
+				<Routes>
+					<Route path="/login" element={<Logins />} />
+					<Route path="/signup" element={<Signup />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
