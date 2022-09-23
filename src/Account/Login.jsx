ea52472e77f4c2f21de4a3299cab256caca432db
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-09-23 01:08:44 +0000

    I  modified the loging form

--- src/Account/Login.jsx
+++ src/Account/Login.jsx
@@ -1,80 +1,55 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Logins = () => {
   return (
 		<>
-			<div className="h-full relative   flex  justify-center min-h-screen overflow-hidden  items-center  sm:px-6 lg:px-8">
-				<div className="max-w-md py-10 shadow-md   bg-white w-full space-y-5">
+			<div className="h-full relative mx-20  flex  justify-center min-h-screen overflow-hidden  items-center  sm:px-6 lg:px-8">
+				<div className="max-w-md  py-10  px-15 shadow-md   bg-white w-full space-y-5">
 					<div>
-						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+						<h2 className=" text-center text-3xl mt-5 mb-0 font-Poppins text-gray-900">
 							Sign in to your account
 						</h2>
 					</div>
-					<form className="mt-8 p-10 space-y-6" action="#" method="POST">
-						<div className="rounded-md  p-30 shadow-sm -space-y-px">
-							
-								<input
-									id="email-address"
-									name="email"
-									type="email"
-									required
-									className="appearance-none  mt-5   rounded-none relative block
-                  w-full px-3 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-t-md
+          {/* FORM */}
+					<form className="mt-2 p-10  space-y-6" action="#" method="POST">
+						<div className="rounded-md  p-30 shadow-sm ">
+            {/* EMAIL */}
+							<input
+								id="email-address"
+								name="email"
+								type="email"
+								required
+								className=" text-lg  font-Poppins relative block
+                  w-full px-10 py-5 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-lg
                   focus:outline-none focus:ring-indigo-500
                   focus:border-indigo-500 focus:z-10 sm:text-sm"
-									placeholder="Email address"
-								/>
-							
-							<div className=''>
+								placeholder="Email address"
+							/>
+              {/* PASSWORD */}
+							<div className="">
 								<input
 									id="password"
 									name="password"
 									type="password"
 									autoComplete="current-password"
 									required
-									className="    mt-5    relative block
-                  w-full px-10 py-2 border border-gray-300
-                  placeholder-gray-500 text-gray-900 rounded-b-md
+									className=" text-lg font-Poppins  mt-5  relative block
+                  w-full px-10 py-4 border border-gray-300
+                  placeholder-gray-500 text-gray-900  rounded-lg
                   focus:outline-none focus:ring-indigo-500
                   focus:border-indigo-500 focus:z-1 sm:text-sm"
 									placeholder="Password"
 								/>
 							</div>
 						</div>
-
-						<div className="flex items-center justify-between">
-							<div className="flex items-center">
-								<input
-									id="remember-me"
-									name="remember-me"
-									type="checkbox"
-									className="h-4 w-4 text-indigo-600 focus:ring-indigo-500
-                  border-gray-300 rounded"
-								/>
-								<label
-									htmlFor="remember-me"
-									className="ml-2 block text-sm text-gray-900"
-								>
-									Remember me
-								</label>
-							</div>
-
-							<div className="text-sm">
-								<a
-									href="/"
-									className="font-medium text-indigo-600 hover:text-indigo-500"
-								>
-									Forgot your password?
-								</a>
-							</div>
-						</div>
-
+             {/* LOGINS BUTTON  */}
 						<div>
 							<button
 								type="submit"
 								className="group relative w-full flex justify-center
-                py-2 px-4 border border-transparent text-sm font-medium
+                py-2 font-Poppins text-lg px-4 border border-transparent  font-medium
                 rounded-md text-white bg-indigo-600 hover:bg-indigo-700
                 focus:outline-none focus:ring-2 focus:ring-offset-2
                 focus:ring-indigo-500"
@@ -83,6 +58,13 @@ const Logins = () => {
 							</button>
 						</div>
 					</form>
+          
+					<p className=" text-lg font-Poppins mb-5 text-center text-gray-700">
+						I don't have account ?{" "}
+						<span className="text-indigo-600">
+							<NavLink to="/signup"> Sing Up </NavLink>
+						</span>
+					</p>
 				</div>
 			</div>
 		</>
