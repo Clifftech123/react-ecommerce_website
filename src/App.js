5f15af2ebdc07118c3ffecc6f2e0c74694610161
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-09-24 21:07:59 +0000

    I added card content for the homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Logins from './Components/Account/Login';
+import Signup from './Components/Account/Singnup';
 import NavBarComponent from './Components/navbarComponent';
-import Logins from "./Account/Login";
-import Signup from './Account/Singnup';
 import HomePage from './Pages/HomePage';
-
-
 
 const App =() => {
   return (
@@ -17,8 +15,8 @@ const App =() => {
 
 				<Routes>
 					<Route path="/" element={<HomePage/>} />
-					<Route path="/login" element={<Logins />} />
-					<Route path="/signup" element={<Signup />} />
+					<Route path="/login" element={ <Logins/> } />
+					<Route path="/signup" element={<Signup/>} />
 				</Routes>
 			</BrowserRouter>
 		</>
