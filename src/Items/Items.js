04642c13b1fd5d3571df70a4887c78e68216b65d
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-10-17 20:44:53 +0000

    I  crated folder for all the items

---
+++ src/Items/Items.js
@@ -0,0 +1,44 @@
+// WELCOME TO OUE SERVICE SECTION INFORMATION 
+   export const Post = [
+	{
+		title: "Product",
+		img: "https://cdn2.mageplaza.com/media/general/ecommerce-product-photo.jpg",
+		content:
+			"react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
+	},
+	{
+		title: "Costumer Support",
+		img: "https://www.revechat.com/wp-content/uploads/2022/02/customer-support-2-1280x720.jpg",
+		content:
+			"react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
+	},
+	{
+		title: " Constutation",
+		img: "https://wecarecomputers.com/wp-content/uploads/2018/02/consultation.jpg",
+		content:
+			"react tailwind css card with image It is a long established fact that a reader will be distracted by the readable content",
+	},
+];
+
+// MEN'S PRODUCT SECTION INFORMATION 
+ export const MenProductSection = [
+	{
+		img: "https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80",
+		Price: "$200",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1604695573706-53170668f6a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		Price: "$300",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1563630423918-b58f07336ac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		Price: "$500",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1588359348347-9bc6cbbb689e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		Price: "$600",
+	},
+];
