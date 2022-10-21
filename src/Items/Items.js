8e5f28cd6e003b625b0506cf1e0efec4b9656983
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-10-21 20:40:38 +0000

    I added some images

--- src/Items/Items.js
+++ src/Items/Items.js
@@ -1,5 +1,5 @@
-// WELCOME TO OUE SERVICE SECTION INFORMATION 
-   export const Post = [
+// WELCOME TO OUE SERVICE SECTION INFORMATION
+export const Post = [
 	{
 		title: "Product",
 		img: "https://cdn2.mageplaza.com/media/general/ecommerce-product-photo.jpg",
@@ -20,25 +20,64 @@
 	},
 ];
 
-// MEN'S PRODUCT SECTION INFORMATION 
- export const MenProductSection = [
+// MEN'S PRODUCT SECTION INFORMATION
+export const MenProductSection = [
 	{
 		img: "https://images.unsplash.com/photo-1620012253295-c15cc3e65df4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1665&q=80",
 		Price: "$200",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
 	},
 
 	{
 		img: "https://images.unsplash.com/photo-1604695573706-53170668f6a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
 		Price: "$300",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
 	},
 
 	{
-		img: "https://images.unsplash.com/photo-1563630423918-b58f07336ac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		img: "https://images.unsplash.com/photo-1594938291221-94f18cbb5660?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
 		Price: "$500",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
 	},
 
 	{
 		img: "https://images.unsplash.com/photo-1588359348347-9bc6cbbb689e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
 		Price: "$600",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
+	},
+];
+
+//  PRODUCT FOR WOMEN
+export const WomenProduct = [
+	{
+		img: "https://images.unsplash.com/photo-1551621955-fa07d4b1376b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		Price: "$100",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1567290290527-b553f4d97960?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+		Price: "$300",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1553404175-78ecf5776a16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=509&q=80",
+		Price: "$140",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
+	},
+
+	{
+		img: "https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+		Price: "$500",
+		content:
+			"Get a fruit machine and a few boxes of pork scratchings in and you’ve basically completely replicated your local pub. Sticky bar-top and ancient, dubiously-stained carpet not included.",
 	},
 ];
