276ca4a60970f5717c2c9886dc9f76dfbefd6d67
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-02 15:56:38 +0000

    I moved Navbar component to homepage componet

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Logins from './Components/Account/Login';
 import Signup from './Components/Account/Singnup';
-import NavBarComponent from './Components/navbarComponent';
 import HomePage from './Pages/HomePage';
+import Dashboard from './Pages/Dashboard';
 
 const App =() => {
   return (
 		<>
 			<BrowserRouter>
-				<div className="">
-					<NavBarComponent />
-				</div>
 
 				<Routes>
 					<Route path="/" element={<HomePage/>} />
 					<Route path="/login" element={ <Logins/> } />
 					<Route path="/signup" element={<Signup/>} />
+					<Route path = "/dashboard" element={ <Dashboard/> } />
 				</Routes>
 			</BrowserRouter>
 		</>
