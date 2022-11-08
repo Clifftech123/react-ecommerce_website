4a0f0d624821bdcd9e359dfa12a2e0fec461ca0c
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-08 19:32:00 +0000

    I change the navbar background color

--- src/Components/Nav/navbarComponent.jsx
+++ src/Components/Nav/navbarComponent.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FaGithub,FaTwitter } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png"
 
 const NavBarComponent = () => {
 	// TOGGLE
@@ -9,25 +10,23 @@ const NavBarComponent = () => {
 	const Navigate = useNavigate();
 	return (
 		<>
-			{/*================ START  NAVBAR COMPONENT ========================*/}
-			<nav className=" w-full bg-NavbarBgColor font-light text-gray-300 shadow -sm">
+			<nav className=" w-full bg-BgColor font-light  shadow -sm">
 				<div className=" justify-between  px-4 mx-auto lg:max-w-10xl md:items-center md:flex md:px-8 font-Poppins">
 					<div>
 						<div className="flex items-center justify-between py-3 md:py-5 md:block">
-							<h2 className="  text-2xl  font-Roboto text-bntColor ">
-								Adom.com
-							</h2>
-
+							
+								<img src={logo} alt="Logo" className="cursor-pointer  h-9" />
+						
 							{/* TOGGLE */}
 							<div className="md:hidden">
 								<button
-									className="p-2 text-white rounded-md outline-none focus:border-gray-400 focus:border"
+									className="p-2  rounded-md outline-none focus:border-gray-400 focus:border"
 									onClick={() => setNavbar(!navbar)}
 								>
 									{navbar ? (
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
-											className="w-6 h-6"
+											className="w-6 h-6 "
 											viewBox="0 0 20 20"
 											fill="currentColor"
 										>
@@ -59,41 +58,49 @@ const NavBarComponent = () => {
 					</div>
 					<div>
 						<div
-							className={`flex-1  text-lg    text-center pb-3 mt-8 md:block  md:pb-0 md:mt-0 ${
+							className={`flex-1  text-lg text-center pb-3 mt-8 md:block  md:pb-0 md:mt-0 ${
 								navbar ? "block   " : "hidden"
 							}`}
 						>
 							{/* NAV ITEMS */}
-							<ul className="items-center   text-2xl  justify-center space-y-8 md:flex md:space-x-10  md:space-y-0  md:divide-y-0">
-								<li className=" font-Roboto  ">
+							<ul className="items-center  text-2xl  justify-center space-y-8 md:flex md:space-x-10  md:space-y-0  md:divide-y-0">
+								<li className="  ">
 									<NavLink to="/"> Home</NavLink>
 								</li>
-								<li className=" font-Roboto "> <NavLink to="/login"> Men</NavLink> </li>
-								<li className="font-Roboto  "> <NavLink to="/login"> Women  </NavLink>   </li>
-								<li className=" font-Roboto "><NavLink to="/login"> Women  </NavLink>  </li>
+								<li>
+									{" "}
+									<NavLink to="/login"> Men</NavLink>{" "}
+								</li>
+								<li>
+									{" "}
+									<NavLink to="/login"> Women </NavLink>{" "}
+								</li>
+								<li>
+									<NavLink to="/login"> Women </NavLink>{" "}
+								</li>
 								<div>
 									<ul className=" text-2xl  mt-5    md:ml-40   text-center  md:flex justify-end  md:mt-0   md:space-x-6 md:space-y-0">
 										<div className="m-2 ">
 											<span>
 												<button
-													className=" center bg-bntColor p-2 w-40  m-2 rounded-lg   text-white"
+													className=" center  bg-indigo-600 p-2 w-40  m-2 rounded-lg   text-white"
 													onClick={() => Navigate("/login")}
 													type="button"
 												>
-													Login{" "}
+													Login
 												</button>
 											</span>
 										</div>
 										{/* SOCIAL ICONS  */}
 										<li
 											title="Github"
-											className="  hidden lg:block p-4 text-white"
+											className="  hidden lg:block p-4 text-black"
 										>
 											<FaGithub />
 										</li>
 										<li
 											title="tWitter"
-											className=" hidden lg:block   p-4 text-white"
+											className=" hidden lg:block   p-4 text-black"
 										>
 											<FaTwitter />
 										</li>
@@ -104,7 +111,6 @@ const NavBarComponent = () => {
 					</div>
 				</div>
 			</nav>
-			
 		</>
 	);
 };
