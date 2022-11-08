d7fb926a0b0c50d64a6bcda44c3929feebcc85cc
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-08 15:46:40 +0000

    I crated dasborad page and added dasboard component to it

---
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Welcome to the dashboard page
+        </div>
+    );
+}
+
+export default Dashboard;
+
