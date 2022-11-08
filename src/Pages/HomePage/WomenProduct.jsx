da53210285d693556ef452bbebb4d8a1019baa7a
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-08 20:28:52 +0000

    I deleted the social icons

--- src/Pages/HomePage/WomenProduct.jsx
+++ src/Pages/HomePage/WomenProduct.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import { useNavigate } from 'react-router-dom';
 import { WomenProductLinks } from "../../Components/ItemsLinks/Items.js";
 import DescriptionTwo from './DescriptionTwo.jsx';
@@ -7,10 +6,7 @@ import DescriptionTwo from './DescriptionTwo.jsx';
 
 const WomenProduct = () => {
 
-    const Github = "https://github.com/Clifftech123";
-		const Twitter = "https://twitter.com/Clifftech_Dev";
-		const Facebook = "https://web.facebook.com/opokuisaiah.clifford";
-    const Instagram = "https://www.instagram.com/opokuisaiahclifford/";
+
     
     const Navigate = useNavigate();
 
@@ -28,7 +24,7 @@ const WomenProduct = () => {
 				<div className="  h-200 grid gap-2 md:gap-10 lg:grid-cols-4  ">
 					{WomenProductLinks.map((Product, name) => (
 						<div
-							className="w-full Lg:shadow-lg mt-10  rounded-lg shadow-sm border-2   border-lightprimary hover:shadow-lg   align-middle justify-center   lg:max-w-md  bg-white  relative overflow-hidden   "
+							className="w-full  mt-10  rounded-lg shadow-sm border-2   border-lightprimary md:hover:shadow-lg   align-middle justify-center   lg:max-w-md  bg-white  relative overflow-hidden   "
 							key={name}
 						>
 							<img
@@ -37,7 +33,7 @@ const WomenProduct = () => {
 								alt="imag"
 							/>
 							<div className="p-4   ">
-								<h4 className="text-2xl  font-bold mb-5 font-Poppins  text-gray-700   ">
+								<h4 className="text-2xl  font-bold mb-5 font-Poppins  text-gray-700    ">
 									{Product.WomenPrice}
 									<span className=" ml-20"> </span>
 								</h4>
@@ -53,28 +49,7 @@ const WomenProduct = () => {
 									See all
 								</button>
 								{/* SOCIAL ICONS */}
-								<span className="flex justify-between mt-5  font-bold  text-gray-700    text-xl ">
-									<li title="Github" className="   block  ">
-										<a href={Github} target={"_blank"} rel="noreferrer">
-											<FaGithub />
-										</a>
-									</li>
-									<li title="tWitter" className=" block   ">
-										<a href={Twitter} target={"_blank"} rel="noreferrer">
-											<FaTwitter />
-										</a>
-									</li>
-									<li title="Facebook" className=" block  ">
-										<a href={Facebook} target={"_blank"} rel="noreferrer">
-											<FaFacebook />
-										</a>
-									</li>
-									<li title="Instagram" className=" block ">
-										<a href={Instagram} target={"_blank"} rel="noreferrer">
-											<FaInstagram />
-										</a>
-									</li>
-								</span>
+								
 							</div>
 						</div>
 					))}
