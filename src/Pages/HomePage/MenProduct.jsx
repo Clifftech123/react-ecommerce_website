b5da27d90c22d11e2a642ad86016edae75b0c7d8
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-08 20:34:06 +0000

    I deleted the social icons

--- src/Pages/HomePage/MenProduct.jsx
+++ src/Pages/HomePage/MenProduct.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { MenProductSection } from "../../Components/ItemsLinks/Items.js";
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+
 
 const MenProduct = () => {
-    const Github = "https://github.com/Clifftech123";
-		const Twitter = "https://twitter.com/Clifftech_Dev";
-		const Facebook = "https://web.facebook.com/opokuisaiah.clifford";
-		const Instagram = "https://www.instagram.com/opokuisaiahclifford/";
 
 		const Navigate = useNavigate();
   return (
@@ -19,7 +15,7 @@ const MenProduct = () => {
 				<div className="grid h-200  gap-2 md:gap-10 lg:grid-cols-4   ">
 					{MenProductSection.map((Product, name) => (
 						<div
-							className="w-full  mt-10  rounded-lg border-2   border-lightprimary shadow-sm  hover:shadow-lg bg-white  align-middle justify-center  lg:max-w-md     relative overflow-hidden  "
+							className="w-full  mt-10  rounded-lg border-2   border-lightprimary shadow-sm  md:hover:shadow-lg bg-white  align-middle justify-center  lg:max-w-md     relative overflow-hidden  "
 							key={name}
 						>
 							<span className="">
@@ -45,29 +41,7 @@ const MenProduct = () => {
 								>
 									See all
 								</button>
-								{/* SOCIAL ICONS */}
-								<span className="flex justify-between mt-5  font-bold align-bottom text-gray-700     text-xl ">
-									<li title="Github" className="   block  ">
-										<a href={Github} target={"_blank"} rel="noreferrer">
-											<FaGithub />
-										</a>
-									</li>
-									<li title="tWitter" className=" block   ">
-										<a href={Twitter} target={"_blank"} rel="noreferrer">
-											<FaTwitter />
-										</a>
-									</li>
-									<li title="Facebook" className=" block  ">
-										<a href={Facebook} target={"_blank"} rel="noreferrer">
-											<FaFacebook />
-										</a>
-									</li>
-									<li title="Instagram" className=" block ">
-										<a href={Instagram} target={"_blank"} rel="noreferrer">
-											<FaInstagram />
-										</a>
-									</li>
-								</span>
+							
 							</div>
 						</div>
 					))}
