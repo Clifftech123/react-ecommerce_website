5d2a89fed7b9a206151b4b738868f4a8270180f5
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-08 15:48:07 +0000

    I change the items to itemsLinks

---
+++ src/Components/ItemsLinks/Items.js
@@ -0,0 +1,79 @@
+// WELCOME TO OUE SERVICE SECTION INFORMATION
+export const Post = [
+	{
+		title: "Product",
+		img: "https://cdn2.mageplaza.com/media/general/ecommerce-product-photo.jpg",
+		content:
+			"We give you best and affordable product.With the little money,  you will get a quality product for us ",
+	},
+	{
+		title: "Customer Support",
+		img: "https://www.revechat.com/wp-content/uploads/2022/02/customer-support-2-1280x720.jpg",
+		content:
+			"We have 24/7Customer service  support for all our Customers. We will assist you for you to get you choice of product ",
+	},
+	{
+		title: " Consultation",
+		img: "https://wecarecomputers.com/wp-content/uploads/2018/02/consultation.jpg",
+		content:
+			"Our Consultation team will helps you to have the fancy of which product suit your needs",
+	},
+];
+
+// MEN'S PRODUCT SECTION INFORMATION
+export const MenProductSection = [
+	{
+		img: "https://m.media-amazon.com/images/I/917js2UrHsL._AC_UY550_.jpg",
+		Price: "$200",
+		amountMoney: "Price: $20 ",
+		content: "Amazon Essentials Men's Seamless Run Tank Top",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/71GmJ2QlJ+L._AC_UY550_.jpg",
+		amountMoney: "Price : $19 ",
+		content:
+			"Carhartt Men's Flame Resistant Force Loose Fit Midweight Full-Zip Sweatshirt",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/61euZKpotuL._AC_UX569_.jpg",
+		amountMoney: "Price : $30 ",
+		content: "Quiksilver Men's Cane Island Woven Top",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/711isVt+YXL._AC_UY550_.jpg",
+		amountMoney: "Price : $25 ",
+		content: " Aware Men's Stretch Moisture Wicking Active Jacket.",
+	},
+];
+
+//  PRODUCT FOR WOMEN
+export const WomenProductLinks = [
+	{
+		img: "https://m.media-amazon.com/images/I/71xbq1jiNHS._AC_UY550_.jpg",
+		WomenPrice: "$Price: 100",
+		content:
+			" PRETTYGARDEN Long Sleeve Maxi Dress for Women - V Neck Casual Button Down Boho Floral Print Fall Long Dresses",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/A1eWimfb1AL._AC_UY550_.jpg",
+		WomenPrice: "  Price : $27",
+		content: "Amazon Essentials Women's Long-Sleeve Woven Blouse",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/71GxnYMVt1L._AC_UY550_.jpg",
+		WomenPrice: " Price : $20",
+		content:
+			"KAYWIDE Women's Casual Flannel Wool Blend Plaid Shacket Jacket Coat Lapel Button Down.",
+	},
+
+	{
+		img: "https://m.media-amazon.com/images/I/817iTsW0I9L._AC_UY550_.jpg",
+		WomenPrice: " Price : $100",
+		content: "Amazon Essentials Women's Brushed Tech Stretch Full-Zip Hoodie.",
+	},
+];
