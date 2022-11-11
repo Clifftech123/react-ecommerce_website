1f68c7a9a9523a116c44fdccd35498ac7982bb83
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-11 19:27:52 +0000

    I removed the react router dom Browser method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Logins from "./Components/Account/Login";
 import Signup from "./Components/Account/Singnup";
 import HomePage from "./Pages/HomePage/HomePage";
@@ -9,14 +9,14 @@ import Product from "./Pages/Product/Product";
 const App = () => {
 	return (
 		<>
-			<BrowserRouter>
+			
 				<Routes>
 					<Route path="/" element={<HomePage />} />
 					<Route path="/login" element={<Logins />} />
 					<Route path="/signup" element={<Signup />} />
 					<Route path="/product" element={<Product/>} />
 				</Routes>
-			</BrowserRouter>
+			
 		</>
 	);
 };
