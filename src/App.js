043a5fee28bac1026ba70979121a0bf2d4d5d6e5
Opoku Isaiah Clifford <opokuisaiahclifford123@gmail.com>
2022-11-12 21:50:16 +0000

    I added contact routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Logins from "./Components/Account/Login";
 import Signup from "./Components/Account/Singnup";
 import HomePage from "./Pages/HomePage/HomePage";
+import Contact from "./Pages/Product/Contact";
 import Product from "./Pages/Product/Product";
 
 
 const App = () => {
 	return (
 		<>
-			
-				<Routes>
-					<Route path="/" element={<HomePage />} />
-					<Route path="/login" element={<Logins />} />
-					<Route path="/signup" element={<Signup />} />
-					<Route path="/product" element={<Product/>} />
-				</Routes>
-			
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<Logins />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/product" element={<Product />} />
+				<Route path="/contact" element={< Contact />} />
+			</Routes>
 		</>
 	);
 };
